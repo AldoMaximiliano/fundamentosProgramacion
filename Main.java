850df3f371b2b6ed50d2b572f367f1a339bb45fa
Aldo Maximiliano <l24121301@morelia.tecnm.mx>
2024-09-10 22:30:43 -0600

    Tarea 3 de la Unidad II

---
+++ Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+        //SECCION 1 LIBRO FAVORITO
+        System.out.println("Mi libro favorito se llama Cumbres Borrascosas, la autora es Emily Bronte, el año en el"+
+                           " se publicó fue en 2019, es del genero novela/tragedia, y tiene 442 páginas.");
+
+        //SECCION 2 PAIS QUE QUIERO VISITAR
+        String nombrePais = "Italia";
+        String nombreCapital = "Roma";
+        String idiomaOficial = "italiano";
+        double poblacion = 58.94;
+        String moneda = "euro";
+
+        System.out.println("El país que quiero visitar es "+nombrePais+", su capital es "+nombreCapital+
+                           ", en Italia se habla "+idiomaOficial+", tiene una población de "+poblacion+
+                           " millones, y la moneda que se utiliza es el "+moneda+".");
+
+        //SECCION 3 PERSONAJE HISTORICO FAVORITO
+
+        String nombre = "Julio Cesar";
+        String fechaNacimiento = "100 A.C.";
+        String nacionalidad = "romana";
+        String ocupacion = "dictador";
+        String logro = "estableció colonias de ciudadanos y reformó el calendario.";
+        System.out.println("Mi personaje historico favorito es "+nombre+", nació en el "+fechaNacimiento+
+                           ", su nacionalidad fue "+nacionalidad+", fue un "+ocupacion+", mientras fue dictador "+
+                           logro);
+    }
+}
