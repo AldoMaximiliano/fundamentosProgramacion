63685021d27f62bdb61699f260b44f123436162e
Aldo Maximiliano <l24121301@morelia.tecnm.mx>
2024-09-13 22:22:40 -0600

    Tarea 5 de la unidad II

--- Main.java
+++ Main.java
@@ -1,25 +1,51 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //FORMULA 1 RESULTADO ESPERADO: 2.3125
-        double a = 12+8, b = 5-3, c = 6/2, d = 3*4, e = 8/2;
+        Scanner sc = new Scanner (System.in);
 
-        double numeradorUno = a*b-c;
-        double denominadorUno = d+e;
-        double totalUno = numeradorUno/denominadorUno;
-        System.out.println(totalUno);
-        //FORMULA 2 RESULTADO ESPERADO: 7.6
-        double n1 = 9*5, n2 = 15+3, n3 = 8*2, n4 = 12/4, n5 = 7+2, n6 = (6*3)/2, n7 = 2+(3*2)/(4-2), n8 = 5*((8/4)+1), n9 = 6/(2+1);
-        double d1 = (((4+2)*(7-1))/(3+1)), d2 = 3*2, d3 = 5+1, d4 = 8/2, d5 = 3+((6/2)+(7/2)), d6 = 2*(3+(8/4));
+        System.out.println("Ingresa tu calle: ");
+        String calle = sc.nextLine();
+        System.out.println("Ingresa tu numero: ");
+        String numero = sc.nextLine();
+        System.out.println("Ingresa tu colonia: ");
+        String colonia = sc.nextLine();
 
-        double numeradorDos = (n1-n2+n3-n4)*n5-n6/n7+n8-n9;
-        double denominadorDos = (d1+(d2-d3*d4))/(d5+d6);
-        double totalDos = numeradorDos/denominadorDos;
-        System.out.println(totalDos);
-        //FORMULA 3 RESULTADO ESPERADO: 2.3333
-        double w = (6+4)*3, x = 18/2, y = 8-5, z = 3*2;
-        double numeradorTres = w-x;
-        double denominadorTres = y+z;
-        double totalTres = numeradorTres/denominadorTres;
-        System.out.println(totalTres);
-            }
+        System.out.println("Ingresa tu codigo postal: ");
+        String codigoPostal = sc.nextLine();
+        System.out.println("Ingresa tu ciudad: ");
+        String ciudad = sc.nextLine();
+        System.out.println("Ingresa tu estado: ");
+        String estado = sc.nextLine();
+
+        System.out.println("Ingresa el nombre de tu primaria: ");
+        String nombrePrimaria = sc.nextLine();
+        System.out.println("Ingresa la ubicacion de tu primaria: ");
+        String ubicacionPrimaria = sc.nextLine();
+
+
+        System.out.println("Ingresa el nombre de tu secundaria: ");
+        String nombreSecundaria = sc.nextLine();
+        System.out.println("Ingresa la ubicacion de tu secundaria: ");
+        String ubicacionSecundaria = sc.nextLine();
+
+        System.out.println("Ingresa el nombre de tu preparatoria: ");
+        String nombrePreparatoria = sc.nextLine();
+        System.out.println("Ingresa la ubicacion de tu preparatoria: ");
+        String ubicacionPreparatoria = sc.nextLine();
+
+        System.out.println("Ingresa el nombre de tu deporte favorito: ");
+        String deporteFavorito = sc.nextLine();
+        //No se que mas preguntar sobre el deporte :c
+
+        String datosDomicilio = "Tu domicilio es: "+calle+", número "+numero+", colonia "+colonia+", "+codigoPostal+", "
+                                 +ciudad+", "+estado+"\n";
+        String datosEscuela = "Estudiaste en la primaria "+nombrePrimaria+", ubicada en "+ubicacionPrimaria+
+                              ", fuiste a la secundaria "+nombreSecundaria+", en "+ubicacionSecundaria+", y la preparatoria en "
+                             +nombrePreparatoria+", en "+ubicacionPreparatoria;
+
+        String fin = datosDomicilio+datosEscuela+", y tu deporte favorito es "+deporteFavorito;
+
+        System.out.println(fin);
+    }
 }
