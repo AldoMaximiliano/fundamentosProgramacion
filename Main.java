5ec77cf11a74cac917d2dfdecd486e46dd3ee9a4
Aldo Maximiliano <l24121301@morelia.tecnm.mx>
2024-09-19 23:25:26 -0600

    Tarea 9  de la Unidad II

--- Main.java
+++ Main.java
@@ -1,28 +1,57 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        double xUno, xDos, yUno, yDos, xTres, yTres;
+        double xUno = 1, xDos = 4, xTres = 5, yUno = 2, yDos = 6, yTres = 3;
 
-        System.out.println("Ingresa la primer coordenada de x: ");
-        xUno = sc.nextDouble();
-        System.out.println("Ingresa la primer coordeanada de y: ");
-        yUno = sc.nextDouble();
-        System.out.println("Ingresa la segunda coordenada de x: ");
-        xDos = sc.nextDouble();
-        System.out.println("Ingresa la segunda coordenada de y: ");
-        yDos = sc.nextDouble();
+        //Lados
+        double distanciaA = Math.sqrt((Math.pow((xDos - xUno),2))+Math.pow(yDos-yUno,2));
+        double distanciaB = Math.sqrt((Math.pow((xDos - xTres),2))+Math.pow(yDos-yTres,2));
+        double distanciaC = Math.sqrt((Math.pow((xUno - xTres),2))+Math.pow(yUno-yTres,2));
 
-        double distancia = Math.sqrt((Math.pow((xDos - xUno),2))+Math.pow(yDos-yUno,2));
+        //Perimetro
+        double perimetro = distanciaA+distanciaB+distanciaC;
 
-        double angulo = Math.atan2(yDos - yUno,xDos - xUno);
+        //Area
+        double a,b,c,s,area;
+        a = distanciaA;
+        b = distanciaB;
+        c = distanciaC;
 
-        double anguloGrados = (angulo * 180) / 3.1416;
+        s = (a + b + c) / 2;
+        area = Math.sqrt(s * (s - a) * (s - b) * (s - c));
 
-        System.out.println("La distancia entre los puntos es: "+distancia);
-        System.out.println("El angulo entre la linea y el exe x en radianes es: "+angulo);
-        System.out.println("El angulo entre la linea y el exe x en grados es: "+anguloGrados);
+        //Altura desde el vertice
+        double altura = (2 * area) / a;
 
+        //Angulos
+        double cosA = (Math.pow(b,2) + Math.pow(c,2) - Math.pow(a,2)  ) / (2 * b * c);
+        double anguloA = Math.toDegrees(Math.acos(cosA));
+        double cosB = (Math.pow(a,2) + Math.pow(c,2) - Math.pow(b,2)  ) / (2 * a * c);
+        double anguloB = Math.toDegrees(Math.acos(cosB));
+        double cosC = (Math.pow(a,2) + Math.pow(b,2) - Math.pow(c,2)  ) / (2 * a * b);
+        double anguloC = Math.toDegrees(Math.acos(cosC));
+
+        //Es equilatero?
+        boolean esEquilatero = Math.abs(a - b) + Math.abs(b - c) + Math.abs(c - a) == 0;
+
+        //Puntos medios
+        double puntoABX = (xUno+xDos) / 2;
+        double puntoABY = (yUno+yDos) / 2;
+        double puntoBCX = (xDos+xTres) / 2;
+        double puntoBCY = (yDos+yTres) / 2;
+        double puntoCAX = (xUno+xTres) / 2;
+        double puntoCAY = (yUno+yTres) / 2;
+        System.out.println("Lado A: "+distanciaA);
+        System.out.println("Lado B: "+distanciaB);
+        System.out.println("Lado C: "+distanciaC);
+        System.out.println("El perimetro es: "+perimetro);
+        System.out.println("El área es: "+area);
+        System.out.println("La altura es: "+altura);
+        System.out.println("Angulo A: "+anguloA+" grados");
+        System.out.println("Angulo B: "+anguloB+" grados");
+        System.out.println("Angulo C: "+anguloC+" grados");
+        System.out.println("El triangulo es equilatero? "+esEquilatero);
+        System.out.println("Punto medio del lado AB: ("+puntoABX+"), ("+puntoABY+")");
+        System.out.println("Punto medio del lado BC: ("+puntoBCX+"), ("+puntoBCY+")");
+        System.out.println("Punto medio del lado CA: ("+puntoCAX+"), ("+puntoCAY+")");
     }
 }
