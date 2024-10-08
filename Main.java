19ea72b0503e5cb0a568f4578a84f7909b99614d
Aldo Maximiliano <l24121301@morelia.tecnm.mx>
2024-10-07 21:22:13 -0600

    Tarea 10 de la Unidad II

--- Main.java
+++ Main.java
@@ -1,51 +1,15 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
+        String cadena = "supercalifragilisticoespialidoso";
 
-        System.out.println("Ingresa tu calle: ");
-        String calle = sc.nextLine();
-        System.out.println("Ingresa tu numero: ");
-        String numero = sc.nextLine();
-        System.out.println("Ingresa tu colonia: ");
-        String colonia = sc.nextLine();
-
-        System.out.println("Ingresa tu codigo postal: ");
-        String codigoPostal = sc.nextLine();
-        System.out.println("Ingresa tu ciudad: ");
-        String ciudad = sc.nextLine();
-        System.out.println("Ingresa tu estado: ");
-        String estado = sc.nextLine();
-
-        System.out.println("Ingresa el nombre de tu primaria: ");
-        String nombrePrimaria = sc.nextLine();
-        System.out.println("Ingresa la ubicacion de tu primaria: ");
-        String ubicacionPrimaria = sc.nextLine();
-
-
-        System.out.println("Ingresa el nombre de tu secundaria: ");
-        String nombreSecundaria = sc.nextLine();
-        System.out.println("Ingresa la ubicacion de tu secundaria: ");
-        String ubicacionSecundaria = sc.nextLine();
-
-        System.out.println("Ingresa el nombre de tu preparatoria: ");
-        String nombrePreparatoria = sc.nextLine();
-        System.out.println("Ingresa la ubicacion de tu preparatoria: ");
-        String ubicacionPreparatoria = sc.nextLine();
-
-        System.out.println("Ingresa el nombre de tu deporte favorito: ");
-        String deporteFavorito = sc.nextLine();
-        //No se que mas preguntar sobre el deporte :c
-
-        String datosDomicilio = "Tu domicilio es: "+calle+", número "+numero+", colonia "+colonia+", "+codigoPostal+", "
-                                 +ciudad+", "+estado+"\n";
-        String datosEscuela = "Estudiaste en la primaria "+nombrePrimaria+", ubicada en "+ubicacionPrimaria+
-                              ", fuiste a la secundaria "+nombreSecundaria+", en "+ubicacionSecundaria+", y la preparatoria en "
-                             +nombrePreparatoria+", en "+ubicacionPreparatoria;
-
-        String fin = datosDomicilio+datosEscuela+", y tu deporte favorito es "+deporteFavorito;
-
-        System.out.println(fin);
+        System.out.println("Longitud: " + cadena.length());
+        System.out.println("Caracter en posicion: " + cadena.charAt(10));
+        System.out.println("En mayusculas: " + cadena.toUpperCase());
+        System.out.println("Subcadena (5,15): " + cadena.substring(5,15));
+        System.out.println("Primera ocurrencia de 'i': " + cadena.indexOf('i'));
+        System.out.println("Ultima ocurrencia de 'o': " + cadena.lastIndexOf('o'));
+        System.out.println("En minusculas: " + cadena.toLowerCase());
+        System.out.println("Subcadena (7,final): " + cadena.substring(7));
+        System.out.println("Ultima ocurrencia de la frase 'li': "+cadena.lastIndexOf("li"));
     }
 }
